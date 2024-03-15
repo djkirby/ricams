6c875e588a88e2169f7b7666813a26f26479873b
Dylan Kirby <dylan@mojotech.com>
2024-03-15 12:43:04 -0400

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/ricams/",
   plugins: [
     remix({
+      basename: "/ricams/",
       ssr: false,
     }),
     tsconfigPaths(),
